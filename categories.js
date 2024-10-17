c5cbf7fd4d6e69a6816d2249b7a49984f3620b3d
JuliuszA-frend <116384338+JuliuszA-frend@users.noreply.github.com>
2024-10-17 22:05:42 +0200

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,29 @@
+const categoriesData = {
+    categories: [
+        {
+            category_id: "301",
+            products: [
+                { products_id: "26774" },
+                { products_id: "26773" },
+                { products_id: "26772" },
+                { products_id: "26664" },
+                { products_id: "26382" },
+                { products_id: "26286" },
+                { products_id: "26018" },
+                { products_id: "25970" },
+                { products_id: "25873" },
+                { products_id: "25812" },
+                { products_id: "25638" },
+                { products_id: "25634" },
+                { products_id: "24150" },
+                { products_id: "24147" },
+                { products_id: "24143" },
+                { products_id: "24142" },
+                { products_id: "24140" },
+                { products_id: "24094" },
+                { products_id: "24028" },
+                { products_id: "23672" }
+            ]
+        }
+    ]
+};

86db6f0f7927e026ea33b150f69a2ccbd140b5ac
JuliuszA-frend <116384338+JuliuszA-frend@users.noreply.github.com>
2025-01-19 00:09:52 +0100

    new version

--- takczytam_new.js
+++ takczytam_new.js
@@ -1693,31 +1693,38 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
   
   document.addEventListener("DOMContentLoaded", function (event) {
     const productElement = document.getElementById("rc_pid");
-    const productId = productElement.getAttribute("data-id");
-    console.log(`Znaleziono ID produktu: ${productId}`);
-    const requestBody = JSON.stringify({
-      products: [{ products_id: productId }],
-    });
-    window.fetch &&
-      fetch("/ajax.php?p=conversion&f=products_info&ssl=1", {
-        method: "POST",
-        body: requestBody,
-        credentials: "same-origin",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
+  
+    // Sprawdzamy, czy element istnieje
+    if (productElement) {
+      const productId = productElement.getAttribute("data-id");
+      console.log(`Znaleziono ID produktu: ${productId}`);
+      
+      const requestBody = JSON.stringify({
+        products: [{ products_id: productId }],
+      });
+  
+      if (window.fetch) {
+        fetch("/ajax.php?p=conversion&f=products_info&ssl=1", {
+          method: "POST",
+          body: requestBody,
+          credentials: "same-origin",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        })
         .then((response) => response.json())
         .then((response) => {
           console.log("Odpowiedź z serwera:", response);
           const categoryIds = [];
+  
           if (
             response.products &&
             Array.isArray(response.products) &&
             response.products.length > 0
           ) {
             const productData = response.products[0];
+  
             if (
               productData.categoriesPaths &&
               Array.isArray(productData.categoriesPaths)
@@ -1734,6 +1741,7 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
           } else {
             console.log("Brak produktów w odpowiedzi.");
           }
+  
           console.log("Id kategorie:", categoryIds);
           const compatibleCategoryIds = getCompatibleCategories(categoryIds);
           console.log("Kompatybilne kategorie:", compatibleCategoryIds);
@@ -1745,6 +1753,11 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
             error
           );
         });
+      }
+    } else {
+      console.log("Element z ID rc_pid nie istnieje na tej stronie.");
+    }
+  
     function getCompatibleCategories(categoryIds) {
       const uniqueCompatibleCategoryIds = new Set();
       categoryIds.forEach((categoryId) => {
@@ -1755,25 +1768,21 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
       });
       return Array.from(uniqueCompatibleCategoryIds);
     }
+  
     function fetchCompatibleCategories(compatibleCategoryIds) {
-      console.log(
-        `Rozpoczynam pobieranie produktów dla unikalnych kompatybilnych kategorii: ${compatibleCategoryIds}`
-      );
+      console.log(`Rozpoczynam pobieranie produktów dla unikalnych kompatybilnych kategorii: ${compatibleCategoryIds}`);
       compatibleCategoryIds.forEach((categoryId) => {
         fetchCategoryAndProducts(categoryId);
       });
     }
+  
     function fetchCategoryAndProducts(categoryId) {
       console.log(`Pobieranie produktów dla kategorii: ${categoryId}`);
-      const category = categoriesData.categories.find(
-        (cat) => cat.category_id === categoryId
-      );
+      const category = categoriesData.categories.find((cat) => cat.category_id === categoryId);
       if (category) {
         console.log(`Znaleziono kategorię: ${categoryId}`, category);
         if (category.products && category.products.length > 0) {
-          console.log(
-            `Liczba produktów w kategorii ${categoryId}: ${category.products.length}`
-          );
+          console.log(`Liczba produktów w kategorii ${categoryId}: ${category.products.length}`);
           fetch("/ajax.php?p=conversion&f=categories_info&ssl=1", {
             method: "POST",
             body: JSON.stringify({
@@ -1787,23 +1796,17 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
               "Content-Type": "application/json",
             },
           })
-            .then((response) => {
-              console.log(
-                `Odpowiedź serwera dla kategorii ${categoryId}:`,
-                response
-              );
-              return response.json();
-            })
-            .then((data) => {
-              console.log(`Dane kategorii ${categoryId}:`, data);
-              displayCategoriesAndProducts(data);
-            })
-            .catch((error) => {
-              console.error(
-                `Błąd podczas pobierania danych dla kategorii ${categoryId}:`,
-                error
-              );
-            });
+          .then((response) => {
+            console.log(`Odpowiedź serwera dla kategorii ${categoryId}:`, response);
+            return response.json();
+          })
+          .then((data) => {
+            console.log(`Dane kategorii ${categoryId}:`, data);
+            displayCategoriesAndProducts(data);
+          })
+          .catch((error) => {
+            console.error(`Błąd podczas pobierania danych dla kategorii ${categoryId}:`, error);
+          });
         } else {
           console.log(`Brak produktów dla kategorii ${categoryId}.`);
         }
@@ -1811,14 +1814,18 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
         console.log(`Nie znaleziono kategorii o ID: ${categoryId}`);
       }
     }
+  
     let allDisplayedProducts = new Set();
+    
     function displayCategoriesAndProducts(data) {
       const container = document.createElement("div");
       container.className = "carousel-container";
+  
       if (data.categories && data.categories.length > 0) {
         data.categories.forEach((category) => {
           const categoryElement = document.createElement("div");
           categoryElement.className = "category";
+  
           const categoryName = document.createElement("h2");
           const customCategoryName = categoryNames[category.category_id];
           if (customCategoryName) {
@@ -1826,38 +1833,30 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
           } else {
             categoryName.textContent = `${category.name_pl} - Może zainteresuje Cię jeszcze coś z tej kategorii`;
           }
+  
           categoryElement.appendChild(categoryName);
+  
           const productsContainer = document.createElement("div");
           productsContainer.className = "products-carousel";
+  
           if (category.products && category.products.length > 0) {
             category.products.forEach((product) => {
-              console.log(
-                `Produkt ID: ${product.products_id}, Nazwa: ${product.name_pl}, Ilość: ${product.quantity}`
-              );
+              console.log(`Produkt ID: ${product.products_id}, Nazwa: ${product.name_pl}, Ilość: ${product.quantity}`);
             });
-            const availableProducts = category.products.filter(
-              (product) => product.quantity > 0
-            );
-            console.log(
-              `Znaleziono ${availableProducts.length} produktów dostępnych w kategorii ${category.name_pl}.`
-            );
+  
+            const availableProducts = category.products.filter((product) => product.quantity > 0);
+            console.log(`Znaleziono ${availableProducts.length} produktów dostępnych w kategorii ${category.name_pl}.`);
+  
             availableProducts.forEach((product) => {
-              console.log(
-                `Produkt ID: ${product.products_id}, Nazwa: ${product.name_pl}, Ilość: ${product.quantity}`
-              );
+              console.log(`Produkt ID: ${product.products_id}, Nazwa: ${product.name_pl}, Ilość: ${product.quantity}`);
             });
+  
             const randomProducts = getRandomProducts(availableProducts, 8);
-            console.log(
-              `Wylosowano ${randomProducts.length} produktów z kategorii ${category.name_pl}.`
-            );
+            console.log(`Wylosowano ${randomProducts.length} produktów z kategorii ${category.name_pl}.`);
+  
             randomProducts.forEach((product) => {
-              if (
-                product.products_id !== productId &&
-                !allDisplayedProducts.has(product.products_id)
-              ) {
-                console.log(
-                  `Wyświetlam produkt: ${product.products_id} - ${product.name_pl}`
-                );
+              if (product.products_id !== productId && !allDisplayedProducts.has(product.products_id)) {
+                console.log(`Wyświetlam produkt: ${product.products_id} - ${product.name_pl}`);
                 const productElement = createProductElement(product);
                 productsContainer.appendChild(productElement);
                 allDisplayedProducts.add(product.products_id);
@@ -1866,68 +1865,92 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
           } else {
             console.log(`Brak produktów w kategorii ${category.name_pl}.`);
           }
+  
           categoryElement.appendChild(productsContainer);
           container.appendChild(categoryElement);
         });
       } else {
         console.log("Brak kategorii w danych.");
       }
+  
       document.getElementById("content").appendChild(container);
     }
+  
     function createProductElement(product) {
       const productElement = document.createElement("div");
       productElement.className = "product-box";
+  
       const productLink = document.createElement("a");
       productLink.href = product.url;
+  
       const productImage = document.createElement("img");
       productImage.src = product.main_image;
       productImage.alt = product.name_pl;
       productImage.className = "product-image";
+  
       productLink.appendChild(productImage);
       productElement.appendChild(productLink);
+  
       const productContent = document.createElement("div");
       productContent.className = "custom-product-box-content";
+  
       const productNameLink = document.createElement("a");
       productNameLink.href = product.url;
+  
       const productName = document.createElement("h3");
       productName.textContent = product.name_pl;
       productNameLink.appendChild(productName);
+  
       productContent.appendChild(productNameLink);
+  
       const productPrice = document.createElement("p");
       productPrice.textContent = `Cena: ${product.promotions_price_brutto} PLN`;
       productPrice.className = `custom-container-price`;
+  
       productContent.appendChild(productPrice);
+  
       const marketPrice = document.createElement("p");
       const marketPriceText = document.createElement("span");
       marketPriceText.textContent = `${product.gross_price} PLN`;
       marketPriceText.style.textDecoration = "line-through";
       marketPrice.textContent = "Cena rynkowa: ";
       marketPrice.appendChild(marketPriceText);
+  
       productContent.appendChild(marketPrice);
+  
       const addToCartButton = createAddToCartButton(product);
       productContent.appendChild(addToCartButton);
+  
       productElement.appendChild(productContent);
+  
       return productElement;
     }
+  
     function createAddToCartButton(product) {
       const addToCartButton = document.createElement("a");
       addToCartButton.id = "custom-add-to-cart";
       addToCartButton.setAttribute("data-product-id", product.products_id);
       addToCartButton.setAttribute("data-item-count", "1");
       addToCartButton.className = "add-to-cart-button";
+  
       const buttonText = document.createElement("span");
       buttonText.textContent = "Dodaj do koszyka";
+  
       addToCartButton.appendChild(buttonText);
+  
       const cartIcon = document.createElement("span");
       cartIcon.className = "mdi mdi-shopping";
       cartIcon.style.marginLeft = "10px";
+  
       addToCartButton.appendChild(cartIcon);
+  
       addToCartButton.addEventListener("click", function () {
         const pid = this.getAttribute("data-product-id");
         const ct = this.getAttribute("data-item-count");
         const item = ct > 0 ? ct : 1;
         const pict = "fb";
         console.log(`Dodawanie produktu ${pid} do koszyka. Ilość: ${item}`);
+  
         new Request({
           method: "post",
           url: "/ajax/pinfo/addcart/",
@@ -1941,10 +1964,8 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
           },
           onComplete: function (data) {
             if (!data) {
-              console.log(
-                `Nie znaleziono danych dla produktu ${pid}. Przekierowanie.`
-              );
-              window.location.href = "/p/0/" + pid + "/.html";
+              console.log(`Nie znaleziono danych dla produktu ${pid}. Przekierowanie.`);
+              window.location.href = `/p/0/${pid}/.html`;
             } else {
               const d = data.split("~");
               console.log(`Odpowiedź z serwera po dodaniu do koszyka: ${data}`);
@@ -1960,9 +1981,9 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
                     title: def.index.window_opt_title,
                     contentHTML: d[1],
                   });
-                  document.getElementById("rc_window_opt_body").innerHTML =
-                    d[1];
+                  document.getElementById("rc_window_opt_body").innerHTML = d[1];
                   break;
+  
                 default:
                   items_cart("products_quantity" + ct + "_" + pid, 0, ct, pict);
                   break;
@@ -1971,8 +1992,10 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
           },
         }).send();
       });
+  
       return addToCartButton;
     }
+  
     function getRandomProducts(products, count) {
       const shuffled = products.slice();
       for (let i = shuffled.length - 1; i > 0; i--) {
@@ -1982,6 +2005,7 @@ if (cenaElement1) { // Sprawdzenie, czy cenaElement1 istnieje
       return shuffled.slice(0, count);
     }
   });
+  
 
   
   if (window.location.href === "https://takczytam.com/_cart/index/") {
